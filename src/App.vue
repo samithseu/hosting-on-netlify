daa7d32fab69fa63aef7b1490042dbb7d9c7084a
Samith Seu <59063392+samithseu@users.noreply.github.com>
2024-10-10 23:42:22 +0700

    initial commits

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,7 @@ export default {
   <nav>
     <button @click="changeLocale('en')">English</button>
     <button @click="changeLocale('km')">ភាសាខ្មែរ</button>
-    <button @click="toggleLocale">Toggle Locale</button>
+    <button @click="toggleLocale">Toggle Language</button>
   </nav>
   <p>{{ $t("welcome") }}</p>
   <p>{{ $t("message") }}</p>

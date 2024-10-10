d268ee92089f78c6825ae51994b643a721ab930d
Samith Seu <59063392+samithseu@users.noreply.github.com>
2024-10-11 00:35:41 +0700

    changing word

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,7 @@ export default {
   <nav>
     <button @click="changeLocale('en')">English</button>
     <button @click="changeLocale('km')">ភាសាខ្មែរ</button>
-    <button @click="toggleLocale">ផ្លាស់ប្ដូររររររររររររ</button>
+    <button @click="toggleLocale">ប្ដូរភាសា</button>
   </nav>
   <p>{{ $t("welcome") }}</p>
   <p>{{ $t("message") }}</p>
